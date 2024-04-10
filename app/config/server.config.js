48c2270e38392a2d8b3a7443d389e9ff41b5cdad
Jhoan Zerpa <jz_22@msn.com>
2024-04-10 12:29:18 -0400

    config server config

--- app/config/server.config.js
+++ app/config/server.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   //RUTA LOCAL
-  HOST: "http://localhost:4000",
+  //HOST: "http://localhost:4000",
 
 
   //RUTA SERVER
   //HOST: "https://mbipi.tresidea.cl"
+  HOST: "https://mbipi-61d04.web.app"
 
 };
